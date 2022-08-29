54a6f3afc28afa4bc3b38587add66e6fc392a055
Joe <joe@icshop.com.tw>
2022-08-29 11:44:27 +0800

    This file is for user manual to snapshot.

---
+++ ros2_ws/src/practice/practice/hello_ros2_node_with_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+class MyNode(Node):
+
+    def __init__(self):
+        super().__init__("hello_ros2")
+        self.get_logger().info("Hello ROS2")
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MyNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
